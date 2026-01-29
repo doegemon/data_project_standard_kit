b9752c6ac7d7ac10313cb4fab3683ec9f757d23b
Nicolas Doege <github.repsu@passinbox.com>
2026-01-29 16:22:54 -0300

    adding extract file and input data

---
+++ app/pipeline/extract.py
@@ -0,0 +1,26 @@
+import glob
+import os
+import pandas as pd
+from typing import List
+
+
+def extract_from_excel(input_path: str) -> List[pd.DataFrame]:
+    """
+    Function to read Excel files from a specific folder, convert them to pandas Dataframes, and append these dataframes into a list.
+
+    args: input_path (str): file path with the Excel files
+
+    return: Dataframes list
+    """
+    files = glob.glob(os.path.join(input_path, "*.xlsx"))
+
+    if not files:
+        raise ValueError("No Excel files found in the specified file path.")
+    df_list = [pd.read_excel(file) for file in files]
+
+    return df_list
+
+
+if __name__ == "__main__":
+    dataframe_list = extract_from_excel("../data/input")
+    print(dataframe_list)
